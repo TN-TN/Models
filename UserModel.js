540e72c81bfb62ce0355d1471f71efb26f2b1702
Tom Sacher <tomnik104@yahoo.de>
2017-03-06 11:19:14 +0100

    Updated User Model

--- UserModel.js
+++ UserModel.js
@@ -2,11 +2,11 @@
  * Created by tom on 04.03.17.
  */
 
-var thinky = require('thinky')();
-var type = thinky.type;
+const thinky = require('thinky')();
+let type = thinky.type;
 
 /**
- * ID - UserID
+ * ID - UserID as UUID v4
  * AccountName - max.mustermann
  * PrimaryEmail - max.mustermann@schule.de
  * emails - Some private mails / verified - has he clicked the verifies link for this mail
@@ -21,21 +21,49 @@ var type = thinky.type;
  */
 
 const User = thinky.createModel('User', {
-    "id": type.number().required(),
+    "id": type.number().uuid(4).required(),
     "account_name": type.string().required(),
     "primary_email": type.string().email(),
     "emails": type.array().schema(type.object().schema({
         "email": type.string().email(),
         "verified": type.boolean()
     })),
-    "createdAt": type.Date().default(r.now()),
+    "createdAt": type.Date().default(r.now()).required(),
+    "createdBy": type.number().required(),
     "permissions": type.array().schema(type.object().schema({
         "id": type.string()
+    })).required(),
+    "private_data": type.array().scheme(type.object().scheme({
+        "address": type.array().scheme(type.object().scheme({
+            "street": type.string(),
+            "number": type.string(),            //String because 5B or similar
+            "zip": type.number(),
+            "city": type.string(),
+            "country": type.string(),
+        })),
+        "phone_number": type.number(),
+        "mobile_number": type.number(),
+        //Erziehungsberechtigte
+        "leagal_guardian": type.array().scheme(type.object().schema({
+            "type": type.array().schema(type.object().schema({
+                "email": type.string().email(),
+                "private_number": type.number(),
+                "mobile_number": type.number(),
+                "address": type.array().scheme(type.object().scheme({
+                    "street": type.string(),
+                    "number": type.string(),            //String because 5B or similar
+                    "zip": type.number(),
+                    "city": type.string(),
+                    "country": type.string(),
+                })),
+            }))
+        })).required(),
+        "notes": type.string().optional()
     })),
     "groups": type.array().schema(type.object().schema({
         "id": type.number()
     })),
-    "profilePicture": type.any(),
+    "profilePicture": type.any().optional(),
     "friends": type.array().schema(type.object().schema({
         "id": type.number()
     })),
